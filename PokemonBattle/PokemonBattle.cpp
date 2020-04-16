9f0ac9b1e82e491efc53d3efeee5bdf467705b57
Kay <kayvanderlans@gmail.com>
2020-04-16 16:13:41 +0200

    Yes

--- PokemonBattle/PokemonBattle.cpp
+++ PokemonBattle/PokemonBattle.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string Starters[3] = { "Bulbasaur", "Squirtle", "Charmander" };
+    
+    std::cout << "Hello Trainer What will your starter be?\n";
+    std::cout << "Will it be:\n";
+ 
+    for (int i = 0; i < 3; i++) {
+        std::cout << Starters[i] << "\n";
+    }
 }
 
